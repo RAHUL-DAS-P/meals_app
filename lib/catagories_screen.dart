09e9b1cd67a3869550a3e82e235c23311f74f7d5
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-03-03 15:36:21 +0530

    adding dummy data by model

---
+++ lib/catagories_screen.dart
@@ -0,0 +1,25 @@
+import "package:flutter/material.dart";
+import 'category_grid.dart';
+import './dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      children: DUMMY_CATEGORIES
+          .map((e) => CateGrid(
+                color: e.color,
+                title: e.title,
+              ))
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 1.5,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+    );
+  }
+}
